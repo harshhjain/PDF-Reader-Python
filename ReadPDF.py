1f029d336b0f8fc219c830be3808b8f50194acc5
Harsh Vardhan Jain <66672238+harshhjain@users.noreply.github.com>
2020-10-30 19:08:15 +0530

    Update ReadPDF.py

--- ReadPDF.py
+++ ReadPDF.py
@@ -1,12 +1,12 @@
 import pyttsx3
 import PyPDF2
 
-myPDF = open('ANcXMLGuide.pdf', 'rb')       # ANcXMLGuide.pdf is the fileName you want to read
-                                            # rb = Read Binary as we are opening PDF file
+myPDF = open('XXXXXX.pdf', 'rb')            # XXXXXX.pdf is the fileName you want to read
+                                            # rb = Read Binary as we have opened PDF file
 pdfReader = PyPDF2.PdfFileReader(myPDF)
 pages = pdfReader.numPages                  # Total number of pages in this PDF
 speak = pyttsx3.init() 
-for number in range(22, pages):             # (22, pages) - 22 is start page till total number of pages in this PDF
+for number in range(22, pages):             # (22, pages) --> 22 is start page till total number of pages in this PDF
     page = pdfReader.getPage(number)
     text = page.extractText()               # Extract into text
     speak.say(text)                         # Speak / say our loud the extracted text
